a9d4c2e8e2e37bd338a9854ca528a4040ad22067
Michael Gregoire <mdgregoire@gmail.com>
2018-02-23 12:04:31 -0600

    talking to giphy

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,28 @@
 const app = angular.module('myApp',[])
+const apiKey = 'KsGqHnc5Rp9pURi4iIEBX4ILYTzvv39L';
+const giphyURL = 'https://api.giphy.com/v1/gifs';
 
 const SearchController = app.controller('SearchController', ['$http', function($http){
   let self = this;
+
+
+  self.searchGiphy = function (searchBar){
+    console.log(searchBar);
+    $http({
+      method: 'GET',
+      url: `${giphyURL}/search?q=${searchBar}&api_key=${apiKey}`
+    })
+    .then(function(response){
+      console.log('success in search', response);
+
+    })
+    .catch(function(error){
+      console.log('error in search', error);
+    })
+  }//end searchGiphy
+
+
+
 
   console.log('inside SearchController');
 }]);//end SearchController
